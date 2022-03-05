4c462182601eea729e32eacf26861e29039dc909
Sipho-sovuyo <siphosovuyomanxoyi@gmail.com>
2022-03-05 22:39:30 +0200

    Modify message class to meet protocol

--- src/message_class.py
+++ src/message_class.py
@@ -1,8 +1,17 @@
 # Class for Messages.
 from gram_class import Gram
 class Message:
+    # Gram 'headers' for protocol
+    blank = 0
+    connect = 1
+    data = 2
+    disconnect = 3
+    ack = 4
     def __init__(self):
         self.body = ""
+        self.ack = False
+        self.type = 0
+        self.id = ''
 
     def unpack_from_gram(self, gram: Gram):
         payload = gram.payload.decode()
@@ -16,3 +25,8 @@ class Message:
 
     def __str__(self):
         return self.body
+
+    def create_message(self, body, type):
+        self.body = body
+        self.type = type
+        
