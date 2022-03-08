2230b8cd7cfc86d763856cac029adb2f9cf3c7d9
Keeran Bezuidenhout <Keeran Bezuidenhout>
2022-03-09 01:25:08 +0200

    Created address book to keep track of connected hosts.

---
+++ src/address_book_util.py
@@ -0,0 +1,14 @@
+# Util for server to keep track of addresses of connected users
+from transmission_util import server_public_address
+from transmission_util import server_recv_port
+
+address_book = {"SERVER": [server_public_address, server_recv_port]}
+
+
+def add_to_address_book(user: str, ip_address: str, port: int):
+    address = [ip_address, port]
+    address[user.upper()] = address
+
+
+def remove_from_address_book(user: str):
+    address_book.pop(user.upper())
