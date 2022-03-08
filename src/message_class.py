f3cd280274d6518b384f3950938842fe8237d057
Keeran Bezuidenhout <Keeran Bezuidenhout>
2022-03-09 00:42:20 +0200

    Updated Message class for serialization. Created util for messages.

--- src/message_class.py
+++ src/message_class.py
@@ -1,38 +1,23 @@
 # Class for Messages.
 from uuid import uuid1
 from gram_class import Gram
+
+
 class Message:
-    # Gram 'headers' for protocol
-    blank = 0
-    connect = 1
-    data = 2
-    disconnect = 3
-    ack = 4
-    def __init__(self):
-        self.body = ""
-        self.ack = False
-        self.type = 0
-        self.id = ''
-        self.source = None
-        self.destination = None
+    # Static variables that represent different types of messages
+    BLANK = 0
+    ACK = 1
+    CONNECT = 2
+    DISCONNECT = 3
+    PING = 4
 
-    def unpack_from_gram(self, gram: Gram):
-        payload = gram.payload.decode()
-        self.body = payload # there should be a method to separe body from headers
-
-    def get_body(self) -> str:
-        return self.body
-
-    def set_body(self, body):
+    def __init__(self, action=0, source_user="", target_user="", message_id=str(uuid1()), body=""):
         self.body = body
+        self.action = action
+        self.message_id = message_id
+        self.source_user = source_user
+        self.target_user = target_user
 
-    def __str__(self):
-        return self.body
-
-    def create_message(self, body, type, source, destination):
-        self.body = body
-        self.type = type
-        self.id = str(uuid1())
-        self.source = source
-        self.destination = destination  
-        
+    def __repr__(self):
+        rep = f'Message(action={self.action}, source_user="{self.source_user}", target_user="{self.target_user}", message_id="{self.message_id}", body="{self.body}")'
+        return rep
--- src/message_util.py
+++ src/message_util.py
@@ -1,12 +1,41 @@
+from log_util import print_message
 from queue_util import recv_q
 from queue_util import send_q
+from message_class import Message
+from gram_class import Gram
+import transmission_util
 
-import message_class
+
+def create_blank_message(source_user: str):
+    """Create a Message object."""
+    return Message(source_user=source_user)
+
+
+def send_message(message: Message):
+    """Send message to specified user."""
+    gram = Gram()
+    address = find_destination_address(message)
+    gram.destination_address = address[0]
+    gram.destination_port = address[1]
+    gram.payload = repr(message).encode()
+    send_q.put(gram)
+
 
 def get_message():
-    """Get a message off the queue."""
-    gram = recv_q.get()
-    message = message_class.Message()
-    message.unpack_from_gram(gram)
+    """Return a message from the queue."""
 
-    return message
+
+def find_destination_address(message: Message):
+    """ Returns a tuple of the appropriate destination address and port for this message."""
+    if message.target_user.upper() == "SERVER".upper():
+        return (transmission_util.server_public_address, transmission_util.server_recv_port)
+
+    # if it's to a user there needs to be a lookup of users addreses
+
+def main():
+    print_message("Message Util")
+    print(repr(create_blank_message("keeran")))
+
+
+if __name__ == "__main__":
+    main()
